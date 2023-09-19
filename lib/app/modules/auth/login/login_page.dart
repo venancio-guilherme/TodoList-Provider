696057e72b7a0c5682e7df44ea4fb2cc667f3597
Guilherme Venancio <guilhermesousav2@gmail.com>
2023-09-19 15:57:26 -0300

    Inserção do Form e dos Buttons na LoginPage

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/widget/todo_list_logo.dart';
 import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
@@ -17,14 +19,90 @@ class LoginPage extends StatelessWidget {
               constraints: BoxConstraints(
                   minHeight: constraints.maxHeight,
                   minWidth: constraints.maxWidth),
-              child: const IntrinsicHeight(
+              child: IntrinsicHeight(
                   child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
-                  TodoListLogo(),
+                  const TodoListLogo(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40.0, vertical: 20.0),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          TextFormField(),
+                          const SizedBox(height: 20),
+                          TextFormField(),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text('Esqueceu sua senha?'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                )),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text('Login'),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xffF0F3F7),
+                        border: Border(
+                          top: BorderSide(
+                            width: 2,
+                            color: Colors.grey.withAlpha(50),
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          SignInButton(
+                            Buttons.Google,
+                            padding: const EdgeInsets.all(5),
+                            shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none),
+                            onPressed: () {},
+                            text: 'Continue com o Google',
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('Não tem conta?'),
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text('Cadastre-se'),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               )),
             ),
